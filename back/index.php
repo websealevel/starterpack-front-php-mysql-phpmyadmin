003a2f0afe11840c1696d52960e098920dbb5247
paul <paul@genesis.websealevel.com*>
2022-03-30 10:44:03 +0200

    ajout test front-back et back-db. Fix db config

--- back/index.php
+++ back/index.php
@@ -1,3 +1,20 @@
 <?php
 
-print "Hello World !";
+// A retirer, juste pour tester le lancement du pack
+
+// Test connexion à la base de données
+$mysqli = new mysqli("db", "root", "root", "information_schema");
+$db_response = $mysqli->ping();
+$mysqli->close();
+
+
+//Retourne une réponse au front
+$data = array("Hello World ! " . match ($db_response) {
+    false => 'La connexion à la base de données a échoué...',
+    true => 'La connexion à la base de données a réussi !'
+});
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+echo json_encode($data);
+exit;
