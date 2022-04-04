1c20b997f756e494a15c0b087581891521662d4d
paul <paul@genesis.websealevel.com*>
2022-04-04 07:34:21 +0200

    change mysqli to PDO, config front et back avec nom du projet

--- back/index.php
+++ back/index.php
@@ -1,4 +1,17 @@
 <?php
 
 // A retirer, juste pour tester le lancement du pack
-header("Access-Control-Allow-Origin: http://PROJECT_NAME.demo.test");
+//CORS policy
+header("Access-Control-Allow-Origin: http://front.PROJECT_NAME.test");
+header("Content-Type: application/json");
+
+$dsn = 'mysql:dbname=information_schema;host=mysql';
+
+try {
+    $pdo = new PDO($dsn, 'root', 'root');
+    echo json_encode(array('La connexion à la base de données a réussi !'));
+} catch (PDOException $e) {
+    echo json_encode(array('La connexion a la base de données a échoué :('));
+}
+
+exit;
