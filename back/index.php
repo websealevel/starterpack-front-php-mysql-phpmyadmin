21c8658d997e684d9ce7e4eaf2a03cba04a1e45e
paul <paul@genesis.websealevel.com*>
2022-04-04 06:29:29 +0200

    config front et back avec nom du projet

--- back/index.php
+++ back/index.php
@@ -1,20 +1,4 @@
 <?php
 
 // A retirer, juste pour tester le lancement du pack
-
-// Test connexion à la base de données
-$mysqli = new mysqli("db", "root", "root", "information_schema");
-$db_response = $mysqli->ping();
-$mysqli->close();
-
-
-//Retourne une réponse au front
-$data = array("Hello World ! " . match ($db_response) {
-    false => 'La connexion à la base de données a échoué...',
-    true => 'La connexion à la base de données a réussi !'
-});
-
-header('Access-Control-Allow-Origin: *');
-header("Content-Type: application/json");
-echo json_encode($data);
-exit;
+header("Access-Control-Allow-Origin: http://PROJECT_NAME.demo.test");
--- init-project.sh
+++ init-project.sh
@@ -5,28 +5,25 @@
 # Permet d'initialiser le nom du projet
 # Cree la base de données et lance le projet
 
-if [ $# -eq 0 ]
-then 
+if [ $# -eq 0 ]; then
   echo "Donnez un nom au projet"
   exit 1
 fi
 
-echo "Nom du projet: $1" 
+echo "Nom du projet: $1"
 
 # Création du fichier .env à partir de .env.dist
 
 FILE_ENV_DIST=.env.dist
 FILE_ENV=.env
 
-if [ ! -f "$FILE_ENV_DIST" ]
-then 
+if [ ! -f "$FILE_ENV_DIST" ]; then
   echo "Le fichier $FILE_ENV_DIST n'existe pas"
   exit 1
 fi
 
 echo "Création du fichier .env"
 cp "$FILE_ENV_DIST" "$FILE_ENV"
-
 
 # Ecriture du nom du projet pour Docker
 
@@ -35,10 +32,16 @@ DOCKER_VARIABLES=(
 )
 
 echo "Configuration du fichier .env"
-for variable in ${DOCKER_VARIABLES[@]}
-do
+
+for variable in ${DOCKER_VARIABLES[@]}; do
   sed -i "s/$variable=[a-zA-Z]*/$variable=$1/" $FILE_ENV
 done
+
+echo "Configuration du front"
+sed -i "s/PROJECT_NAME/$1/" front/index.html
+
+echo "Configuration du back"
+sed -i "s/PROJECT_NAME/$1/" back/index.php
 
 # Lancement des conteneurs
 echo "Lancement du projet"
